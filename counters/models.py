6dadc57670b8a1e97af9d5bce18b6c3a0748f55e
midiblack <midiblack@yandex-team.ru>
2019-11-18 23:56:30 +0300

    Add new table

--- counters/models.py
+++ counters/models.py
@@ -9,10 +9,28 @@ db = SQLAlchemy()
 ma = Marshmallow()
 
 
-class Counter(db.Model):
+class Place(db.Model):
+    __tablename__ = 'places'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(250), nullable=False)
+    geo = db.Column(db.String(64))
+    adress = db.Column(db.String(250), nullable=False)
+
+    def __init__(self, description, geo, adress):
+        self.description = description
+        self.geo = geo
+        self.adress = adress
+
+class Counters(db.Model):
     __tablename__ = 'counters'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(250), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(64))
+    unit_of_measurement = db.Column(db.String(32), nullable=False)
+    place_id = db.Column(db.Integer)
 
-    def __init__(self, name):
+    def __init__(self, description, name, unit_of_measurement, place_id):
+        self.description = name
         self.name = name
+        self.unit_of_measurement = unit_of_measurement
+        self.place_id = place_id
